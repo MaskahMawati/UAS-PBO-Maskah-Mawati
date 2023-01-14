d056e6b2e0d868278a39a24f2e8f7836715ce6c7
MaskahMawati <maskahmawati@gmail.com>
2023-01-14 17:55:58 +0700

    UAS

---
+++ FDA_E020320012/app/src/main/java/id/ac/poliban/mi/va/maskah/fda_e020320012/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.va.maskah.fda_e020320012.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ WisataApp_E020320012/app/src/main/java/id/ac/poliban/mi/va/maskah/wisataapp_e020320012/decoration/OnClickLayoutMarginItemListener.java
@@ -0,0 +1,10 @@
+package id.ac.poliban.mi.va.maskah.wisataapp_e020320012.decoration;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface OnClickLayoutMarginItemListener {
+    void onClick(Context context, View v, int position, int spanIndex, RecyclerView.State state);
+}
